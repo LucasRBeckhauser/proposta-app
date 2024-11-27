f563412a7d5e5cf12d312a7c23a1c8423afdcddd
LucasRBeckhauser <lucasbeckh@live.com>
2024-11-27 12:57:43 -0300

    Configuração de permissão e inicialização das filas no rabbitMQ

--- src/main/java/com/beckhauser/config/RabbitMQConfiguration.java
+++ src/main/java/com/beckhauser/config/RabbitMQConfiguration.java
@@ -4,6 +4,10 @@ package com.beckhauser.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -32,5 +36,19 @@ public class RabbitMQConfiguration {
     public Queue criarFilaPropostaConcluidaMsNotificacao () {
         return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
     }
+
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin (ConnectionFactory connectionFactory) {
+    return new RabbitAdmin(connectionFactory);
+}
+
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializarAdmin (RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
+    
     
 }
