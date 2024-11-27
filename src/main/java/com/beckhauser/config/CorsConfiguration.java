f247b5cd58634b23b1a9495cb60d848eca920e30
LucasRBeckhauser <lucasbeckh@live.com>
2024-11-27 12:38:39 -0300

    Mapeamento das filas no RabbitMQ

--- src/main/java/com/beckhauser/config/CorsConfiguration.java
+++ src/main/java/com/beckhauser/config/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.beckhauser.proposta_app.mapper;
+package com.beckhauser.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
